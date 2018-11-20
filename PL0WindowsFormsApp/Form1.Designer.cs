3ffa14fd41c9a6051b185c0f46f4562a6f77dcea
xuzhida <1459601448@qq.com>
2018-11-20 10:08:37 +0800

    add some files

---
+++ PL0WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace PL0WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxfile = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.choosefile = new System.Windows.Forms.Button();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.col1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonsavefile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // textBoxfile
+            // 
+            this.textBoxfile.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxfile.Location = new System.Drawing.Point(46, 34);
+            this.textBoxfile.Name = "textBoxfile";
+            this.textBoxfile.Size = new System.Drawing.Size(678, 30);
+            this.textBoxfile.TabIndex = 1;
+            this.textBoxfile.TextChanged += new System.EventHandler(this.textBoxfile_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(1042, 696);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // choosefile
+            // 
+            this.choosefile.Location = new System.Drawing.Point(810, 33);
+            this.choosefile.Name = "choosefile";
+            this.choosefile.Size = new System.Drawing.Size(88, 30);
+            this.choosefile.TabIndex = 3;
+            this.choosefile.Text = "选择文件";
+            this.choosefile.UseVisualStyleBackColor = true;
+            this.choosefile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxCode.Location = new System.Drawing.Point(46, 125);
+            this.textBoxCode.Multiline = true;
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxCode.Size = new System.Drawing.Size(470, 491);
+            this.textBoxCode.TabIndex = 4;
+            this.textBoxCode.TextChanged += new System.EventHandler(this.textBoxCode_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.col1,
+            this.col2,
+            this.col3});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(538, 125);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(360, 342);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // col1
+            // 
+            this.col1.Text = "单词";
+            this.col1.Width = 100;
+            // 
+            // col2
+            // 
+            this.col2.Text = "类型";
+            this.col2.Width = 100;
+            // 
+            // col3
+            // 
+            this.col3.Text = "值";
+            this.col3.Width = 100;
+            // 
+            // buttonsavefile
+            // 
+            this.buttonsavefile.Location = new System.Drawing.Point(46, 88);
+            this.buttonsavefile.Name = "buttonsavefile";
+            this.buttonsavefile.Size = new System.Drawing.Size(90, 31);
+            this.buttonsavefile.TabIndex = 7;
+            this.buttonsavefile.Text = "保存文件";
+            this.buttonsavefile.UseVisualStyleBackColor = true;
+            this.buttonsavefile.Click += new System.EventHandler(this.buttonsavefile_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(538, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 31);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "词法分析";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(538, 510);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(360, 106);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(534, 470);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "错误";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "行数";
+            this.columnHeader1.Width = 140;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "错误";
+            this.columnHeader2.Width = 140;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 700);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonsavefile);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBoxCode);
+            this.Controls.Add(this.choosefile);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBoxfile);
+            this.Name = "Form1";
+            this.Text = "PL0词法分析器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxfile;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button choosefile;
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button buttonsavefile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader col1;
+        private System.Windows.Forms.ColumnHeader col2;
+        private System.Windows.Forms.ColumnHeader col3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ PL0WindowsFormsApp/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PL0WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string[] key = { "const", "var", "procedure", "odd", "if", "then", "else", "while", "do", "call", "begin", "end", "read", "write", "repeat", "until" };
+        public string[] op = { "+", "-", "*", "/", "=", "odd", "<", "<=", ">", ">=", ":=", "<>" };
+        public string file, code;
+        public List<List<string>> wordresult = new List<List<string>>();//保存词法分析结果
+        public List<List<string>> errorlist = new List<List<string>>();
+        public int line = 1;
+        public int error = 0;
+        public char ch;//每次读取的字符
+        public char temp;//缓冲字符
+        public string token;//每次读取的单词
+        public int sym;//每次读取的单词的类别
+        public string value;//每次读取的单词的值
+        public FileStream fs;
+        public StreamReader sr;
+        public bool flag = false;
+
+        //将单词加入到结果中
+        public void addresult(string t, int s, string v)
+        {
+            List<string> tk;
+            switch (s)
+            {
+                case 1:
+                    tk = new List<string> { t, "关键字", v };
+                    break;
+                case 2:
+                    tk = new List<string> { t, "标识符", v };
+                    break;
+                case 3:
+                    tk = new List<string> { t, "运算符", v };
+                    break;
+                case 4:
+                    tk = new List<string> { t, "常数", v };
+                    break;
+                default:
+                    tk = new List<string> { t, "分界符", v };
+                    break;
+            }
+            wordresult.Add(tk);
+        }
+        //加入错误列表
+        public void adderror(int id)
+        {
+            List<string> el;
+            switch (id)
+            {
+                case 1:
+                    el = new List<string> { line.ToString(), ":后面必须为=" };
+                    break;
+                case 2:
+                    el = new List<string> { line.ToString(), "其它错误" };
+                    break;
+                default:
+                    el = new List<string> { line.ToString(), "error" };
+                    break;
+            }
+            errorlist.Add(el);
+        }
+        //判断是否为关键字
+        public bool iskey(string v)
+        {
+            foreach (string i in key)
+            {
+                if (i == v)
+                    return true;
+            }
+            return false;
+        }
+        //每次读取一个字符
+        public int getch()
+        {
+            if (flag)
+            {
+                flag = false;
+                // Console.Write(ch);
+                return 1;
+            }
+            else
+            {
+                int content = sr.Read();
+                if (content != -1)
+                {
+                    ch = Convert.ToChar(content);
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        //+,-,*,/，;,等符号
+        public void addop(char c, int s)
+        {
+            token = Convert.ToString(c);
+            value = token;
+            //  Console.WriteLine(token, s, value);
+            addresult(token, s, value);
+        }
+
+        //判断字符是数字字母还是都不是
+        public int judge(char ch)
+        {
+            int t = ch - '0';
+            if (t >= 0 && t <= 9)
+                return 1;
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+                return 2;
+            else
+                return 0;
+        }
+
+        //每次读取一个单词
+        public int getsym()
+        {
+            token = "";
+            value = "";
+            getch();
+            while (ch == ' ' || ch == '\n' || ch == '\t')
+            {
+                if (ch == '\n')
+                {
+                    line++;
+                }
+                getch();
+            }
+            if (sr.Peek() == -1)
+            {
+                return 0;
+            }
+            if (judge(ch) == 2)
+            {
+                while (judge(ch) != 0)
+                {
+                    token = token + ch;
+                    getch();
+                }
+                value = token;
+                temp = ch;
+                flag = true;
+                fs.Seek(-1, SeekOrigin.Current);//回退鼠标，因为已经将光标移到前面一个了
+                if (iskey(token))
+                    sym = 1;//1是关键字
+                else
+                    sym = 2;//2是标识符
+                addresult(token, sym, value);
+            }
+            else if (judge(ch) == 1)
+            {
+                while (judge(ch) == 1)
+                {
+                    token = token + ch;
+                    getch();
+                }
+                Console.WriteLine(ch);
+                if (ch == '.')
+                {
+                    token = token + '.';
+                    getch();
+                    while (judge(ch) == 1)
+                    {
+                        token = token + ch;
+                        getch();
+                    }
+                    value = token;
+                }
+                else
+                {
+                    value = Convert.ToString(int.Parse(token), 2);
+                }
+                sym = 4;
+                temp = ch;
+                flag = true;
+                fs.Seek(-1, SeekOrigin.Current);
+                addresult(token, sym, value);
+            }
+            else if (ch == ':')
+            {
+                getch();
+                if (ch == '=')
+                {
+                    sym = 3;
+                    token = ":=";
+                    value = ":=";
+                    addresult(token, sym, value);
+
+                }
+                else
+                {
+                    sym = 3;
+                    token = ":";
+                    value = ":";
+                    addresult(token, sym, value);
+                    flag = true;
+                    adderror(1);
+                }
+            }
+            //else if (ch == '=')
+            //{
+            //    sym = 3;
+            //    token = "=";
+            //    value = "=";
+            //    addresult(token, sym, value);
+            //}
+            else if (ch == '<')
+            {
+                sym = 3;
+                getch();
+                if (ch != '=')
+                {
+                    flag = true;
+                    fs.Seek(-1, SeekOrigin.Current);
+                    token = "<";
+                    value = "<";
+                    temp = ch;
+                }
+                else
+                {
+                    token = "<=";
+                    value = "<=";
+                }
+                addresult(token, sym, value);
+            }
+            else if (ch == '>')
+            {
+                sym = 3;
+                getch();
+                if (ch != '=')
+                {
+                    flag = true;
+                    fs.Seek(-1, SeekOrigin.Current);
+                    token = ">";
+                    value = ">";
+                    temp = ch;
+                }
+                else
+                {
+                    token = ">=";
+                    value = ">=";
+                }
+                addresult(token, sym, value);
+            }
+            else if (ch == '+' || ch == '-' || ch == '*' || ch == '/'||ch=='=')
+            {
+                token = Convert.ToString(ch);
+                value = token;
+                sym = 3;
+                addresult(token, sym, value);
+                // Console.WriteLine(token, sym, value);
+            }
+            else if (ch == ';' || ch == ',' || ch == '.' || ch == '(' || ch == ')')
+            {
+                token = Convert.ToString(ch);
+                value = token;
+                sym = 5;
+                addresult(token, sym, value);
+                // Console.WriteLine(token, sym, value);
+            }
+            return 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Title = "请选择文件";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                file = fileDialog.FileName;//返回文件的完整路径  
+
+                textBoxfile.Text = file.ToString();
+                textBoxCode.Text = File.ReadAllText(file, Encoding.Default);
+
+            }
+
+        }
+
+        private void textBoxfile_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            fs = new FileStream(file, FileMode.Open);
+            sr = new StreamReader(fs, Encoding.ASCII);
+            wordresult.Clear();//清空之前运行的结果
+            this.listView1.Items.Clear();
+            this.listView1.Scrollable = true;//有滚动条
+            errorlist.Clear();
+            this.listView2.Items.Clear();
+            this.listView2.Scrollable = true;//有滚动条
+            while (getsym() == 1)
+            {
+            }
+            //将最后的.加入
+            if (textBoxCode.Text[textBoxCode.Text.Length - 1] == '.')
+            {
+                List<string> end = new List<string> { ".", "分界符", "." };
+                wordresult.Add(end);
+            }
+            for (int i = 0; i < wordresult.Count(); i++)
+            {
+                ListViewItem item = new ListViewItem(wordresult[i].ToArray());
+                listView1.Items.Add(item);
+            }
+            if (errorlist.Count() > 0)
+            {
+                for (int i = 0; i < errorlist.Count(); i++)
+                {
+                    ListViewItem item = new ListViewItem(errorlist[i].ToArray());
+                    listView2.Items.Add(item);
+                }
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxCode_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonsavefile_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(file, textBoxCode.Text, Encoding.Default);
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
